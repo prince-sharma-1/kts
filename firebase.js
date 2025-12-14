12669f9da66b985a161f4f2ddc7e36a0685491fb
unknown <you@example.com>
2025-12-14 22:09:35 +0530

    js file updated for database with modified api

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
+// Import the functions you need from the SDKs you need
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 // Your Firebase config (replace with your own from Firebase Console)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "ktsacademy.firebaseapp.com",
-  projectId: "ktsacademy",
-  storageBucket: "ktsacademy.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "YOUR_APP_ID"
-};
+     apiKey: "AIzaSyDbovkhsoXdtudKdY3Ixl3KIZ1NXo4KH4k",
+    authDomain: "kts-academy.firebaseapp.com",
+    projectId: "kts-academy",
+    storageBucket: "kts-academy.firebasestorage.app",
+    messagingSenderId: "474382144386",
+    appId: "1:474382144386:web:4969617fec5522ad52359b",
+    measurementId: "G-CRV48HGH35"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -46,3 +50,16 @@ export async function loginUser(email, password) {
     alert("Error: " + error.message);
   }
 }
+<script type="module">
+  
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = 
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
+</script>   
